281621858d82ae04ac06bbd49a6065dd1b90bb34
Young Choe <ydchoe@ucsc.edu>
2019-10-16 23:25:29 -0700

    added class to store food details and SQLiteDBHelper

---
+++ Android/app/src/main/java/com/project/kitt/FoodDetail.java
@@ -0,0 +1,38 @@
+package com.project.kitt;
+
+public class FoodDetail
+{
+    private int foodID;
+    private String foodName;
+    private String foodDate;
+
+    public int getFoodID()
+    {
+        return foodID;
+    }
+
+    public void setFoodID(int foodID)
+    {
+        this.foodID = foodID;
+    }
+
+    public String getFoodName()
+    {
+        return foodName;
+    }
+
+    public void setFoodName(String foodName)
+    {
+        this.foodName = foodName;
+    }
+
+    public String getFoodDate()
+    {
+        return foodDate;
+    }
+
+    public void setFoodDate(String foodDate)
+    {
+        this.foodDate = foodDate;
+    }
+}
